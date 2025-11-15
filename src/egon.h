9be81568364f2822590b6953be0709d582ee0311
Robert J. Hansen <rob@hansen.engineering>
2025-11-15 14:25:08 -0500

    Added Apache license.

--- src/egon.h
+++ src/egon.h
@@ -1,6 +1,18 @@
-//
-// Created by Robert J. Hansen on 11/14/25.
-//
+/*
+Copyright 2025, Robert J. Hansen <rjh@sixdemonbag.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 #ifndef EGON_FETCH_H
 #define EGON_FETCH_H
@@ -10,21 +22,13 @@
 
 class egon_error : public std::runtime_error {
 public:
-    egon_error() noexcept
-        : std::runtime_error("")
-    {
-    }
-    explicit egon_error(const char* what) noexcept
-        : std::runtime_error(what)
-    {
-    }
-    explicit egon_error(const std::string& what) noexcept
-        : std::runtime_error(what.c_str())
-    {
-    }
-    ~egon_error() noexcept override { }
+  egon_error() noexcept : std::runtime_error("") {}
+  explicit egon_error(const char *what) noexcept : std::runtime_error(what) {}
+  explicit egon_error(const std::string &what) noexcept
+      : std::runtime_error(what.c_str()) {}
+  ~egon_error() noexcept override {}
 };
 
-void fetch_key_for(const char* addr);
+void fetch_key_for(const char *addr);
 
 #endif // EGON_FETCH_H
--- src/fetch.cpp
+++ src/fetch.cpp
@@ -1,3 +1,19 @@
+/*
+Copyright 2025, Robert J. Hansen <rjh@sixdemonbag.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
 #include "egon.h"
@@ -15,43 +31,52 @@ using std::format;
 using std::string;
 using std::unique_ptr;
 
-void fetch_key_for(const char* addr)
-{
-    static bool isGpgMEInitialized = false;
-    static httplib::Client client { "https://api.protonmail.ch" };
+namespace {
+  string get_certificate_for(const char* addr) {
+    static httplib::Client client{"https://api.protonmail.ch"};
 
-    // All networking code lives here.
     auto res = client.Get(format("/pks/lookup?op=get&search={}", addr));
     switch (res.error()) {
-    case httplib::Error::Success:
-        break;
-    case httplib::Error::SSLConnection:
+      case httplib::Error::Success:
+        return res->body;
+      case httplib::Error::SSLConnection:
         throw egon_error(format("SSL connection failed: {}", res.ssl_error()));
-    case httplib::Error::SSLLoadingCerts:
-    case httplib::Error::SSLServerVerification:
-    case httplib::Error::SSLServerHostnameVerification:
-        throw egon_error(format("SSL hostname verification failed, X509 error: {}",
-            res.ssl_openssl_error()));
-    default:
+      case httplib::Error::SSLLoadingCerts:
+      case httplib::Error::SSLServerVerification:
+      case httplib::Error::SSLServerHostnameVerification:
+        throw egon_error(format("SSL general failure, X509 error: {}",
+                                res.ssl_openssl_error()));
+      default:
         throw egon_error(format("HTTP error: {}", httplib::to_string(res.error())));
     }
+  }
 
-    // All GnuPG code lives here.
+  void import_certificate(const std::string& certificate) {
+    static bool isGpgMEInitialized = false;
+
     if (!isGpgMEInitialized) {
-        GpgME::initializeLibrary();
-        isGpgMEInitialized = true;
+      GpgME::initializeLibrary();
+      isGpgMEInitialized = true;
     }
-    const unique_ptr<Context> ctx { Context::createForProtocol(OpenPGP) };
+
+    const unique_ptr<Context> ctx{Context::createForProtocol(OpenPGP)};
     if (nullptr == ctx) {
-        throw egon_error("Couldn’t acquire context.");
+      throw egon_error("Couldn’t acquire context.");
     }
-    auto buffer = Data(res->body.c_str(), res->body.size(), true);
+
+    auto buffer = Data(certificate.c_str(), certificate.size(), true);
     if (buffer.isNull()) {
-        throw egon_error("Couldn’t allocate buffer");
-    };
+      throw egon_error("Couldn’t allocate buffer");
+    }
+
     auto errcode = ctx->importKeys(buffer);
     if (errcode.error().isError()) {
-        throw egon_error(format("Couldn’t import key: {}",
-            errcode.error().asStdString()));
+      throw egon_error(
+          format("Couldn’t import key: {}", errcode.error().asStdString()));
     }
+  }
+}
+
+void fetch_key_for(const char *addr) {
+  import_certificate(get_certificate_for(addr));
 }
